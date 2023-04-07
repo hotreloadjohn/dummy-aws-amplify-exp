99c6fe2c5efa99ce78eb3bcf6c5b495ed4603e59
John.NG <john.cyber.box@gmail.com>
2023-04-07 13:21:16 +0800

    refactor: added like state

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import artImg from "./assets/images/art.png";
 import "./App.css";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+
   return (
     <div className="App">
       <img src={artImg} className="logo react" alt="art" />
       <h1>Designed by: John Ng</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Like {count}
+        <button onClick={() => setLikes((like) => like + 1)}>
+          Like {likes}
         </button>
       </div>
     </div>
